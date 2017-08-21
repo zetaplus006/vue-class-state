498cda3863cc5e73297c726cd31ece264609246b
zetaplus006 <msz006@yeah.net>
2017-08-21 17:31:02 +0800

    add test

---
+++ test/unit/api.js
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import * as Vubx from '../';
+console.log(Object.keys(Vubx).sort());
+describe('api', function () {
+    it('export apis', function () {
+        expect(Object.keys(Vubx).sort()).to.equal([
+
+        ])
+    })
+})
