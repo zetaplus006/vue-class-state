9e0ae2c34cb9a5c42297eb87de6f94d4c8281cd4
zetaplus006 <msz006@yeah.net>
2017-09-06 20:46:41 +0800

    mutation bug

--- src/service/mutation.ts
+++ src/service/mutation.ts
@@ -8,13 +8,12 @@ export interface IMutation {
 export function mutation(target: any, mutationyKey: string, descriptor: PropertyDescriptor) {
     const mutationFn = descriptor.value;
     descriptor.value = function (this: IService, ...arg: any[]) {
-        const middleware = this.__.middleware,
-            vubxMutation: IMutation = {
-                type: this.__.identifier.toString() + ': ' + mutationyKey,
-                payload: arg
-            };
+        const vubxMutation: IMutation = {
+            type: this.__.identifier.toString() + ': ' + mutationyKey,
+            payload: arg
+        };
         const root = this.__.$root || this as IService;
-
+        const middleware = root.__.middleware;
         const temp = root.__.isCommitting;
         root.__.isCommitting = true;
         let res;
